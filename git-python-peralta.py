1ab13d101f88389bedbdd09e3b15249db2877747
PeraltaChristopher <xtopherastro@gmail.com>
2022-03-29 19:14:30 +0800

    My Hobbies added

--- git-python-peralta.py
+++ git-python-peralta.py
@@ -1,3 +1,8 @@
 print ("Marinduque States College")
 print ("Information Technology")
 print ("Second Year, Section: F")
+print ("Playing Basketball")
+print ("Playing Volleyball")
+print ("Playing Mobile and PC Games")
+print ("Eating")
+print ("Watching Movies")
